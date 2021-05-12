4e2f998489c73d38b5aabda1960ae71fafa06a15
matte <matte@DESKTOP-C6A18PK.lan>
2021-05-12 17:00:37 +0200

    Versione completa

--- src/main/java/it/polito/tdp/crimes/db/DBConnect.java
+++ src/main/java/it/polito/tdp/crimes/db/DBConnect.java
@@ -17,7 +17,7 @@ public class DBConnect {
 			HikariConfig config = new HikariConfig();
 			config.setJdbcUrl(jdbcURL);
 			config.setUsername("root");
-			config.setPassword("");
+			config.setPassword("hitmon5");
 			
 			// configurazione MySQL
 			config.addDataSourceProperty("cachePrepStmts", "true");
